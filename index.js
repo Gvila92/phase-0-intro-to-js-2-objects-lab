413ab0c114a42086f0ca260830d724d320cc016a
George vila <gvila92@gmail.com>
2023-10-05 14:55:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const updatedEmployee = { ...employee };
-    updatedEmployee[key] = value;
-    return updatedEmployee;
-  }
-  
-  const employee = {
-    name: "Sam",
-    age: 30,
-    streetAddress: "11 Broadway",
-  };
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  function deleteFromEmployeeByKey(employee, key) {
-    const updatedEmployee = { ...employee };
-    delete updatedEmployee[key];
-    return updatedEmployee;
-  }
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
-  }
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+const employee = {
+  name: "Sam",
+  age: 30,
+  streetAddress: "11 Broadway",
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
